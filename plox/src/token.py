6a38e0854b64df452f6fd756765182ed8e70179c
timmPrice <timmPrice6@gmail.com>
2025-05-10 21:35:22 -0400

    adding token class

---
+++ plox/src/token.py
@@ -0,0 +1,10 @@
+class Token:
+    def __init__(self, type: TokenType, lexeme: str, literal: object, line: int):
+        self.type = type
+        self.lexeme = lexeme
+        self.literal = literal
+        self.line = line
+
+    def __str__(self) -> str:
+        return(f"{type} {lexeme} {literal} {line}")
+
