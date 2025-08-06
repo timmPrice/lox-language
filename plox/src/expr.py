49bd6dd998b306dd679f401836016f996f2771f9
timmPrice <timmPrice6@gmail.com>
2025-08-06 14:58:40 -0400

    generation of the interfaces

--- plox/src/expr.py
+++ plox/src/expr.py
@@ -1,7 +1,27 @@
 from token import Token
+from abc import ABC, abstractmethod
 
-class Expr:
-    pass
+class Expr(ABC):
+    @abstractmethod
+    def accept(self, visitor: 'Visitor'):
+         pass
+
+class Visitor(ABC):
+    @abstractmethod
+    def visit_binary_expr(self, expr: 'Binary'):
+        pass
+
+    @abstractmethod
+    def visit_grouping_expr(self, expr: 'Grouping'):
+        pass
+
+    @abstractmethod
+    def visit_literal_expr(self, expr: 'Literal'):
+        pass
+
+    @abstractmethod
+    def visit_unary_expr(self, expr: 'Unary'):
+        pass
 
 class Binary(Expr):
     def __init__(self, left: Expr, operator: Token, right: Expr):
