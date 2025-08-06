5a94aee78be53c06809bf8791618a84090efe628
timmPrice <timmPrice6@gmail.com>
2025-08-06 14:22:53 -0400

    generated AST boilerplate

---
+++ plox/src/expr.py
@@ -0,0 +1,24 @@
+from token import Token
+
+class Expr:
+    pass
+
+class Binary(Expr):
+    def __init__(self, left: Expr, operator: Token, right: Expr):
+       self.left = left
+       self.operator = operator
+       self.right = right
+
+class Grouping(Expr):
+    def __init__(self, expression: Expr):
+       self.expression = expression
+
+class Literal(Expr):
+    def __init__(self, value: object):
+       self.value = value
+
+class Unary(Expr):
+    def __init__(self, operator: Token, right: Expr):
+       self.operator = operator
+       self.right = right
+
