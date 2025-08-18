37946b25a7725bb2a9e880278151c244fae90eb9
timmPrice <timmPrice6@gmail.com>
2025-08-18 16:58:30 -0400

    hacking more of the parser together... adding error detection and completing the recursive decent

--- plox/src/parser.py
+++ plox/src/parser.py
@@ -1,6 +1,12 @@
+from src import token_type
 from token_type import TokenType
 from token import Token
-from expr import Binary, Expr
+from lox import error
+from expr import Binary, Expr, Unary, Literal, Grouping
+
+
+class ParserError(RuntimeError):
+    pass
 
 class Parser:
     def __init__(self, tokens: list[Token]):
@@ -11,14 +17,71 @@ class Parser:
         return self.equality()
     
     def equality(self) -> Expr:
-        expr: Expr = comparison()
+        expr: Expr = self.comparison()
 
         while (self.match(Token.BANG_EQUAL, Token.EQUAL_EQUAL)):
             operator: Token = self.previous()
-            right: Expr = comparison()
+            right: Expr = self.comparison()
             expr = Binary(expr, operator, right) 
 
         return expr
+
+    def comparison(self) -> Expr:
+        expr: Expr = self.term()
+        
+        while (self.match(Token.GREATER, Token.GREATER_EQUAL, Token.LESS, Token.LESS_EQUAL)):
+            operator: Token = self.previous()
+            right: Expr = self.term()
+            expr = Binary(expr, operator, right)
+
+        return expr
+
+    def term(self) -> Expr:
+        expr: Expr = self.factor()
+
+        while (self.match(Token.MINUS, Token.PLUS)):
+            operator: Token = self.previous()
+            right: Expr = self.factor()
+            expr = Binary(expr, operator, right)
+
+        return expr
+
+    def factor(self) -> Expr:
+        expr: Expr = self.unary()
+
+        while (self.match(Token.SLASH, Token.STAR)):
+            operator: Token = self.previous()
+            right: Expr = self.unary()
+            expr = Binary(expr, operator, right)
+
+        return expr
+
+    def unary(self) -> Expr:
+
+        if (self.match(Token.BANG, Token.MINUS)):
+            operator: Token = self.previous()
+            right: Expr = self.unary()
+            return Unary(operator, right)
+
+        return self.primary()
+
+    def primary(self) -> Expr:
+        if self.match(Token.FALSE):
+            return Literal(False)
+        if self.match(Token.TRUE):
+            return Literal(True)
+        if self.match(Token.NULL):
+            return Literal(None)
+
+        if (self.match(Token.STRING, Token.NUMBER)):
+            return Literal(self.previous().literal)
+
+        if (self.match(Token.LEFT_PAREN)):
+            expr = self.expression()
+            self.consume(Token.RIGHT_PAREN, "Expect ')' after expression.")
+            return Grouping(expr)
+
+        raise ParserError("Expected Expression")
 
     def match(self, *types: TokenType) -> bool:
         for type in types:
@@ -45,3 +108,12 @@ class Parser:
 
     def previous(self) -> Token:
         return self.tokens[self.current - 1]
+
+    def consume(self, type: TokenType, message: str) -> Token:
+        if (self.check(type)):
+            return self.advance()
+        raise ValueError(self.peek(), message)
+
+    def _error(self, token: Token, message: str): 
+        error(token, message)
+        raise ParserError()
